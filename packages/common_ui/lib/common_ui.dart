aaaa0f808df2f0202e2fda8841b7a61cd6d15888
Bani <thabranirahman@gmail.com>
2022-12-12 10:40:07 +0800

    common_ui: remove flavour banner package and include in common_ui package

--- packages/common_ui/lib/common_ui.dart
+++ packages/common_ui/lib/common_ui.dart
@@ -1,8 +1,12 @@
 library result;
 
+export 'src/bottom_app_bar.dart';
+export 'src/dialog.dart';
+export 'src/environment.dart';
+export 'src/flavor_banner_widget.dart';
 export 'src/info_list_tile.dart';
 export 'src/tapable_text.dart';
 export 'src/text_divider.dart';
-export 'src/bottom_app_bar.dart';
-export 'src/dialog.dart';
 export 'src/text_divider.dart';
+export 'src/unfocus.dart';
+export 'src/version_builder.dart';
--- packages/flavor_banner/lib/flavor_banner.dart
+++
@@ -1,6 +0,0 @@
-library flavor_banner;
-
-export 'src/environment.dart';
-export 'src/flavor_banner_widget.dart';
-export 'src/version_builder.dart';
-export 'src/unfocus.dart';
